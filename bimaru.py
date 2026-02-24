ea4a41cefe1c4b4c6309f5ff2ea53a8865a4570a
1netr0 <alexander8192@web.de>
2026-02-24 15:56:13 +0100

    Bordstruktur, unfertig, invalid syntax

--- bimaru.py
+++ bimaru.py
@@ -5,6 +5,27 @@ pygame.init()
 DISPLAYSURF = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Bimaru")
 
+unbekannt = 0
+wasser = 1
+schiff = 2
+
+reihen = 10
+spalten = 10
+board = [[unbekannt for _ in range(spalten)] for _ in range(reihen)]
+
+def draw_board(surface):
+    for i in range(reihen):
+        for j in range(spalten):
+            if board[i][j] == unbekannt:
+                color = (200, 200, 200)
+            elif board[i][j] == wasser:
+                color = (0, 0, 255)
+            elif board[i][j] == schiff:
+                color = (255, 0, 0)
+
+            pygame.draw.rect(DISPLAYSURF, color, (j*50, i*50, 50, 50), 0)
+
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -12,6 +33,5 @@ while True:
             sys.exit()
     
     DISPLAYSURF.fill((255, 255, 255))
+    draw_board(DISPLAYSURF)
     pygame.display.update()
-
-
