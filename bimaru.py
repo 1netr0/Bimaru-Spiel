d27e10d31f329e958eee8c7e76873cc513fcb295
1netr0 <alexander8192@web.de>
2026-03-03 15:35:47 +0100

    Definierung der Schiffe

--- bimaru.py
+++ bimaru.py
@@ -12,11 +12,41 @@ schiff = 2
 class Feld():
     def __init__(self, status=unbekannt):
         self.status = status
+        self.schiff = None
+
+class Schiff():
+    def __init__(self, laenge, felder):
+        self.laenge = laenge
+        self.felder = felder
+        self.treffer = set()
+    
+    def versenkt(self):
+        return len(self.treffer) == self.laenge
 
 
 reihen = 10
 spalten = 10
 board = [[Feld() for _ in range(spalten)] for _ in range(reihen)]
+
+def in_bound(x, y):
+    return 0 <= x < reihen and 0 <= y < spalten
+
+def kann_platzieren(felder):
+    for x, y in felder:
+        if not in_bound(x, y):
+            return False
+        if board[x][y].status is not unbekannt:
+            return False
+    return True
+
+def platzieren_schiff(x, y, laenge, horizontal):
+    felder = []
+    for k in range (laenge):
+        if horizontal:
+            felder.append((x, y+k))
+        else:
+            felder.append((x+k, y))
+        
 
 def draw_board(surface):
     for i in range(reihen):
